dd4c45f86accd119d65070c7b2db449e112a203f
Fabian Fahrenholz <faf@acadon.de>
2021-11-04 12:41:04 +0100

    Implemented naive write and read functions for nbt

--- nbt/nbt.go
+++ nbt/nbt.go
@@ -9,6 +9,29 @@ import (
 	"reflect"
 )
 
+func Read(reader io.Reader, compressed bool) (Tag, error) {
+	if compressed {
+		r, err := gzip.NewReader(reader)
+		if err != nil {
+			return nil, err
+		}
+
+		return Read(r, false)
+	} else {
+		return NewReader(reader).ReadTag()
+	}
+}
+
+func Write(writer io.Writer, tag Tag, compressed bool) error {
+	if compressed {
+		w := gzip.NewWriter(writer)
+
+		return Write(w, tag, false)
+	} else {
+		return NewWriter(writer).WriteTag(tag)
+	}
+}
+
 func Unmarshal(reader io.Reader, v interface{}) error {
 	for {
 		dataType, err := readByte(reader)
