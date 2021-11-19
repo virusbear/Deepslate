8db60d16e69f3d6103885b0be7cfea9de7435b2d
Fabian Fahrenholz <faf@acadon.de>
2021-11-19 15:48:07 +0100

    Implemented reading data from packet

--- protocol/packet.go
+++ protocol/packet.go
@@ -1,87 +1,168 @@
 package protocol
 
 import (
+	"bytes"
 	"encoding/binary"
-	"io"
+	"errors"
+	"fmt"
 )
 
-type PacketReader struct {
-	io.Reader
-}
+const (
+	MaxStringLength = 32767
+	MaxChatLength = 262144
+	MaxIdentifierLength = 32767
+)
 
 type Packet struct {
 	Id int32
-	Data []byte
+	data *bytes.Buffer
 }
 
-func NewPacketReader(reader io.Reader) *PacketReader {
-	return &PacketReader{reader}
+func (p Packet) read(v interface{}) error {
+	return binary.Read(p.data, binary.BigEndian, v)
 }
 
-func (reader PacketReader) Read(threshold int32) *Packet {
-
-}
-
-func (reader PacketReader) readBool() (bool, error) {
+func (p Packet) ReadBool() (bool, error) {
 	var value byte
-	if err := binary.Read(reader.Reader, binary.BigEndian, &value); err != nil {
+	if err := p.read(value); err != nil {
 		return false, err
 	}
 
 	return value != 0, nil
 }
 
-func (reader PacketReader) readByte() (int8, error) {
+func (p Packet) ReadByte() (int8, error) {
 	var value int8
-	if err := binary.Read(reader.Reader, binary.BigEndian, &value); err != nil {
+	if err := p.read(value); err != nil {
 		return 0, err
 	}
 
 	return value, nil
 }
 
-func (reader PacketReader) readUByte() (uint8, error) {
+func (p Packet) ReadUByte() (uint8, error) {
 	var value uint8
-	if err := binary.Read(reader.Reader, binary.BigEndian, &value); err != nil {
+	if err := p.read(value); err != nil {
 		return 0, err
 	}
 
 	return value, nil
 }
 
-func (reader PacketReader) readShort() (int16, error) {
+func (p Packet) ReadShort() (int16, error) {
 	var value int16
-	if err := binary.Read(reader.Reader, binary.BigEndian, &value); err != nil {
+	if err := p.read(value); err != nil {
 		return 0, err
 	}
 
 	return value, nil
 }
 
-func (reader PacketReader) readUShort() (uint16, error) {
+func (p Packet) ReadUShort() (uint16, error) {
 	var value uint16
-	if err := binary.Read(reader.Reader, binary.BigEndian, &value); err != nil {
+	if err := p.read(value); err != nil {
 		return 0, err
 	}
 
 	return value, nil
 }
 
-func (reader PacketReader) readInt() (int32, error) {
+func (p Packet) ReadInt() (int32, error) {
 	var value int32
-	if err := binary.Read(reader.Reader, binary.BigEndian, &value); err != nil {
+	if err := p.read(value); err != nil {
 		return 0, err
 	}
 
 	return value, nil
 }
 
-func (reader PacketReader) readLong() (int64, error) {
+func (p Packet) ReadLong() (int64, error) {
 	var value int64
-	if err := binary.Read(reader.Reader, binary.BigEndian, &value); err != nil {
+	if err := p.read(value); err != nil {
 		return 0, err
 	}
 
 	return value, nil
 }
 
+func (p Packet) ReadFloat() (float32, error) {
+	var value float32
+	if err := p.read(value); err != nil {
+		return 0, err
+	}
+
+	return value, nil
+}
+
+func (p Packet) ReadDouble() (float64, error) {
+	var value float64
+	if err := p.read(value); err != nil {
+		return 0, err
+	}
+
+	return value, nil
+}
+
+func (p Packet) ReadString() (string, error) {
+	return p.readString(MaxStringLength)
+}
+
+func (p Packet) ReadChat() (string, error) {
+	return p.readString(MaxChatLength)
+}
+
+func (p Packet) ReadIdentifier() (string, error) {
+	return p.readString(MaxIdentifierLength)
+}
+
+func (p Packet) readString(maxLength int) (string, error) {
+	length, err := p.ReadVarInt()
+	if err != nil {
+		return "", err
+	}
+	if int(length) > maxLength * 4 {
+		return "", fmt.Errorf("the received encoded string buffer length is longer than maximum allowed (%d > %d)", length, maxLength * 4)
+	}
+	if length < 0 {
+		return "", errors.New("the received encoded string buffer length is less than zero! Weird string!")
+	}
+
+	value := make([]rune, 0)
+	for size := 0; size < int(length); {
+		r, s, err := p.data.ReadRune()
+		if err != nil {
+			return "", err
+		}
+		size += s
+
+		value = append(value, r)
+	}
+
+	return string(value), nil
+}
+
+func (p Packet) ReadVarInt() (int32, error) {
+	var value int32
+	var offset int
+	var current byte
+
+	for {
+		v, err := p.data.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+
+		value |= (v & 0x7f) << offset * 7
+		offset++
+
+		if offset > 5 {
+			return 0, errors.New("VarInt too big")
+		}
+
+		if (current & 0x80) != 0x80 {
+			break
+		}
+	}
+
+	return value, nil
+}
